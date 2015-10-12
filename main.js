40c75af7edc09883487be34bf84090c0d8327b16
Doron Pagot <doronpagot@gmail.com>
2015-10-12 13:21:41 +0300

    add simple radar-chart animation

--- main.js
+++ main.js
@@ -24,4 +24,33 @@ $(function() {
         // }
     });
 
+    var ctx = document.getElementById("team-radar").getContext("2d");
+    var data = {
+        labels: ["Visual Design", "Software", "Product Development", "Team", "Amazing Product"],
+        datasets: [
+
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [100, 100, 100, 100, 101]
+            }
+        ]
+    };
+    var radar = new Chart(ctx).Radar(data, {
+        scaleShowLabels: false,
+        showTooltips: false
+    });
+
+    setInterval(function(){
+       radar.datasets[0].points.forEach(function(point){
+        point.value = 100 + Math.random()*10
+        })
+        radar.update()
+    }, 800)
+
 });
